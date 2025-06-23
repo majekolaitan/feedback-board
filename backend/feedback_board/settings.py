fd284b7afc064f3afa53bb9447bb4973864eac32
Olaitan Majekodunmi <majekolaitan@gmail.com>
2025-06-23 10:18:27 +0100

    Docker Compose setup

--- backend/feedback_board/settings.py
+++ backend/feedback_board/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-#vytzy2n$9ubvxme!mm5s(^7$3tj4ptjtl#0rhhkp70^i!n01@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '12_7.0.0.1', 'backend']
 
 
 # Application definition
